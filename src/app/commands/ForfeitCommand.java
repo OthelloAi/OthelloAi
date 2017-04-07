70ef9dfaab01a340d55a41c36e7c1ccfece7568e
MartijnSnijder <martijn-snijder@hotmail.com>
2017-04-07 10:51:11 +0200

    Added commands

---
+++ src/app/commands/ForfeitCommand.java
@@ -0,0 +1,12 @@
+package app.commands;
+
+/**
+ * Created by Martijn on 7-4-2017.
+ */
+
+public class ForfeitCommand implements Command {
+    @Override
+    public String toString() {
+        return "forfeit";
+    }
+}
